200b853be0516cb56d8e912e55616a600293be0a
Wyrdix <wyrdix@gmail.com>
2025-03-27 11:59:01 +0100

    Remove Codeinput directly use CodeMirror

--- src/lib/modules/typing/TypingBlock.svelte
+++ src/lib/modules/typing/TypingBlock.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+	import CodeMirror from 'svelte-codemirror-editor';
+	import { functional } from '../../highlighter/FunctionalHighlighter';
+	import { dracula } from '@ddietr/codemirror-themes/dracula';
+	import { functionalLinter } from '../../highlighter/FunctionalHighlighting';
+
 	let {
 		data = $bindable(),
 		editable,
@@ -10,4 +15,12 @@
 	} = $props();
 </script>
 
-<div class="relative">A</div>
+<div class="relative">
+	<CodeMirror
+		bind:value={data}
+		lang={functional()}
+		extensions={[functionalLinter]}
+		theme={dracula}
+		{editable}
+	/>
+</div>
