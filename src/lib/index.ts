3860c4b58afd4751f71f00fef99709a943067b46
Wyrdix <wyrdix@gmail.com>
2025-03-30 23:23:07 +0200

    Fix publishing issues

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,3 @@
 export * from './modules/typing/TypingBlock.svelte';
 export { default } from './modules/typing/TypingBlock.svelte';
-
-export * from './modules/typing/TypingModule';
+export { default as TypingModule } from './modules/typing/TypingModule';
